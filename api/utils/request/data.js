51932ba119a80b1d82d6fe403b4b22a7f83b2de9
Odumosu Oluwashina <odumz20@gmail.com>
2020-11-29 18:14:07 +0100

    subdomain check and passing env

--- api/utils/request/data.js
+++ api/utils/request/data.js
@@ -1,26 +1,26 @@
 const addEntity = () => ({
   method: 'post',
-  url: `${process.env.OCX_HOST_DATA}:${process.env.OCX_PORT_DATA}/table/create`,
+  url: `${process.env.OCX_HOST_DATA}/table/create`,
 });
 
 const insertEntity = () => ({
   method: 'post',
-  url: `${process.env.OCX_HOST_DATA}:${process.env.OCX_PORT_DATA}/query/create`,
+  url: `${process.env.OCX_HOST_DATA}/query/create`,
 });
 
 const removeEntity = () => ({
   method: 'delete',
-  url: `${process.env.OCX_HOST_DATA}:${process.env.OCX_PORT_DATA}/query/delete`,
+  url: `${process.env.OCX_HOST_DATA}/query/delete`,
 });
 
 const rollback = () => ({
   method: 'post',
-  url: `${process.env.OCX_HOST_DATA}:${process.env.OCX_PORT_DATA}/setup`,
+  url: `${process.env.OCX_HOST_DATA}/setup`,
 });
 
 const searchEntity = () => ({
   method: 'get',
-  url: `${process.env.OCX_HOST_DATA}:${process.env.OCX_PORT_DATA}/query/retrieve`,
+  url: `${process.env.OCX_HOST_DATA}/query/retrieve`,
 });
 
 module.exports = {
